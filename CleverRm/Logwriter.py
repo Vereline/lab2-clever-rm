3458dd62365f7eedeb00ec5b8c61702ee5d006bf
Vereline <vstanko1998@gmail.com>
2017-04-05 00:59:25 +0300

    changed the struct of json log

--- CleverRm/Logwriter.py
+++ CleverRm/Logwriter.py
@@ -12,9 +12,10 @@ def write_json_log(file_path):
     path = os.path.abspath(file_path)
     date = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")
     name = os.path.split(file_path)
-    file_dict["name"] = name
+    file_dict["name"] = name[1]
+    file_path["name_directory"] = name[0]
     file_dict["path"] = path
     file_dict["date"] = date
-    filename = unicode(os.path.getatime(os.curdir))
+    filename = unicode(datetime.strftime(datetime.now(), "%Y.%m.%d"))
     json.dump(file_dict, open(filename, 'a'))
     print 'succeed'
